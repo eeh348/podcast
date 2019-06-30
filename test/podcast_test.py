245e26a9eecf7fe07dff7de6e4e1d612abb97751
Eileen Hengel <eileenhengel@Bobs-MacBook-Pro.local>
2019-06-30 10:06:49 -0400

    create file structure

---
+++ test/podcast_test.py
@@ -0,0 +1,21 @@
+#podcast_test.py
+
+#import os
+
+#from omniparser.gradebook_parser import calculate_average_grade_from_csv
+
+#from omniparser.gradebook_parser import (
+#    calculate_average_grade_from_csv,
+#    calculate_average_grade_from_json
+
+
+# CSV VERSION
+#
+
+#def test_calculate_average_grade_from_csv():
+    #gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2019.csv")
+    #assert calculate_average_grade_from_csv(gradebook_filepath) == 90.64
+
+
+    #prev_gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2018.csv")
+    #assert calculate_average_grade_from_csv(prev_gradebook_filepath) == 83.64
