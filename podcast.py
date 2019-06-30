8e49cd05f388c2eb99f67c6213a4f3187e20a780
Eileen Hengel <eileenhengel@Bobs-MacBook-Pro.local>
2019-06-30 09:53:43 -0400

    update podcast.py

--- podcast.py
+++ podcast.py
@@ -5,8 +5,7 @@ import os
 import requests
 import json
 import pprint
-from dotenv import load_dotenv
-import datetime
+import datetime as dt
 import PySimpleGUI as sg
 
 from dotenv import load_dotenv
@@ -14,9 +13,6 @@ from dotenv import load_dotenv
 #sg.Popup("Hello From PySimpleGUI!", "This is the shortest GUI program ever!")
 
 #capture keys
-load_dotenv()
-my_cred = os.environ.get("X_LISTEN_API_KEY")
-
 list_string = []
 new_search = []
 
@@ -27,15 +23,32 @@ def format_search(search):
     return new_search
 
 def format_date(timestamnp):
-    new_timestamp = datetime.datetime.fromtimestamp(timestamp)
+    new_timestamp = dt.datetime.fromtimestamp(timestamp)
     new_timestamp = new_timestamp.strftime('%b %d %Y')
     return new_timestamp
 
-#if __name__ == '__main__': 
+#if __name__ == '__main__':
 
-#ask user for input and make sure it's emppty
+load_dotenv()
+my_cred = os.environ.get("X_LISTEN_API_KEY")
 
-search = input("What topics are you looking to search: ")
+#build search GUI
+layout = [
+    [sg.Text("SEARCH"), sg.InputText()],
+    [sg.OK()]
+]
+
+window = sg.Window("Podcast Search Tool").Layout(layout)
+
+button, values = window.Read()
+
+print(type(values)) #> dict
+print(values) #> {0: 'Polly Professor'}
+search = values[0] #> Polly Professor
+print("SEARCHING ON...:", search)
+
+#ask user for input and make sure it's empty
+#search = input("What topics are you looking to search: ")
 
 if search == "":
     search = input("Your input was blank. Please search a topic: ")
@@ -46,7 +59,7 @@ else:
 new_search = format_search(search)
 
 #fetch data
-request_url = f"https://listen-api.listennotes.com/api/v2/search?q={new_search}&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1390190241000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=1"
+request_url = f"https://listen-api.listennotes.com/api/v2/search?q={new_search}&sort_by_date=0&scope=episode&offset=0&len_min=10&len_max=100&published_before=0&published_after=1390190241000&only_in=title%2Cdescription%20author&language=English&safe_mode=1"
 response = requests.get(request_url, headers={"X-ListenAPI-Key": my_cred})
 parsed_response = json.loads(response.text)
 
@@ -72,35 +85,32 @@ for r in results:
 
     timestamp = timestamp/1000.0
 
-    #new_timestamp = format_date(timestamp)
+#build GUI
+layout = [
+    [sg.Text("RESULTS"), sg.InputText()],
+    [sg.OK()]
+]
+
+window = sg.Window("Podcast Search Tool").Layout(layout)
+
+button, values = window.Read()
+
+print(type(values)) #> dict
+print(values) #> {0: 'Polly Professor'}
+search = values[0] #> Polly Professor
+print("SEARCHING ON...:", search)
+
+#print results
+for r in results:
+    timestamp = r['pub_date_ms']
+    title = r['title_original']
+    podcast = r['podcast_title_original']
+
+    timestamp = timestamp/1000.0
 
     print(f"DATE: {format_date(timestamp)}")
     print(f"TITLE: {title}")
     print(f"DESCRIPTION: {podcast}")
     print("-------------------")
-
-
-
-#print(response)
-#print(parsed_response)
-#print(my_cred)
-
-#capture multiple inputs in a list until user types DONE; does not validate if it's a valid stock symbol
-##while True:
-#    #ask user for stock symbol
-#    search = input("What topics are you looking to search")
-#    symbol = symbol.upper()
-#
-#    if symbol == "DONE":
-#        break
-#    elif symbol.isalpha() and len(symbol) < 6:
-#        #symbol = symbol.upper()
-#        symbol_list.append(symbol.upper())
-#    else:
-#        print("Input must be A-Z characters only and less than or equal to 5 characters")
-#
-#create user input
-
-#price = response.json()
 
 
